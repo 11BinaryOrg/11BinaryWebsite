40e14453bab04351b37c31f0d97c094c7adcce81
Tracy@11Binary.us <Tracy@11Binary.us>
2021-09-12 13:51:09 -0400

    Upload old website

---
+++ Scripts/ChartData.js
@@ -0,0 +1,81 @@
+﻿
+
+
+
+var lineData = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [2180, 1664, 1942, 2231, 2481, 2329, 2471]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [1592, 1232, 1677, 1492, 1657, 2114, 2050]
+            }
+        ]
+    };
+
+    var barData = {
+        labels: ["Tampa", "St. Petersburg", "Clearwater", "Brandon", "Palm Harbor"],
+        datasets: [
+            {
+                label: " Users",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: [1173, 943, 882, 421, 554]
+            },
+            {
+                label: "Followers",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
+                data: [603, 272, 166, 85, 117]
+            }
+        ]
+    };
+    var pieData = [
+                    {
+                        value: 3662,
+                        color: "rgba(220,220,220,0.5)",
+                        highlight: "rgba(220,220,220,0.8)",
+                        label: "blah"
+                    },
+                    {
+                        value: 11601,
+                        color: "rgba(151,187,205,0.5)",
+                        highlight: "rgba(151,187,205,0.8)",
+                        label: "Positive"
+                    },
+
+    ];
+    var pieData2 = [
+                    {
+                        value: 2730,
+                        color: "rgba(220,220,220,0.5)",
+                        highlight: "rgba(220,220,220,0.8)",
+                        label: "Active"
+                    },
+                    {
+                        value: 1243,
+                        color: "rgba(151,187,205,0.5)",
+                        highlight: "rgba(151,187,205,0.8)",
+                        label: "Followers"
+                    },
+
+    ];
---
+++ Scripts/Site.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+
+    $(window).on("load resize", function () {
+        var height = window.innerHeight;
+        $(".full-screen").css("height", height);
+    });
+
+    $('body').scrollspy({
+        target: '.navbar',
+        offset:240
+    });
+
+    $("#send").click(function (event) {
+        event.preventDefault();
+        var name = $("#inputName").val();
+        var email = $("#inputEmail").val();
+        var phone = $("#inputPhone").val();
+        
+        if (name == '' || email == '' || phone == '') {
+        alert("Hey, we can't contact you unless you give us your information, now can we? Now fill out the form!");
+        
+        } else {
+            alert("Message Recieved from " + name + ", " + "email: " + email + ", " + "phone: " + phone);
+        }
+    });
+
+    $('.navbar-header a, ul.nav a, .topIcon a, .linkButton a').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top-100
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
